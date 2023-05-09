1012289a0fb87956b99ef09e88ed1f904e8e2249
Abdelrhman Ahmad Yahia <47979060+AbdelrahmanYahia@users.noreply.github.com>
2023-05-09 09:59:21 +0300

    Add files via upload

---
+++ GUAP.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+import datetime 
+now = datetime.datetime.now()
+start_time = now.strftime("%Y-%m-%d %H:%M:%S")
+import os
+os.environ['start_time'] = start_time
+
+from src.cli import cli
+from src.utils.globals import *
+
+if __name__ == '__main__':
+    cli = cli.Cli()
